5584986c6b495c35cfbc0d19471110924c566db1
Lucy <lucydenisenko@gmail.com>
2022-06-03 15:05:30 -0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/*
+Write a method to sum 2 ints
+Overload it so that you can sum long and long, double and double
+
+Return type - double for all methods
+ */
+
+
 public class Main {
     public static void main(String[] args) {
         Int i = new Int ();
